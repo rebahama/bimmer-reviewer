57deb8fd23fbade4045e98443d5cd70ec9cd9afe
rebahama <wiri@hotmail.se>
2022-07-05 23:53:36 +0000

    add delete function to post

--- bimmerreviewer/views.py
+++ bimmerreviewer/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Post
 
 
 class Firstview(ListView):
+    """Show all the data from the queryset """
     model = Post
     template_name = 'home.html'
 
 
 class DetailReview(DetailView):
+    """ Only showing the detalied view when clicked
+    on the "Firstview"""
     model = Post
     template_name = 'detail-review.html'
 
 
 class CreateReview(CreateView):
+    """ Create a new view and render it in the template below"""
     model = Post
     template_name = 'create-review.html'
     fields = '__all__'
 
 
 class UpdateReview(UpdateView):
+    """ Update the view and the fileds that are below
+    """
     model = Post
     template_name = 'update-review.html'
     fields = ['title', 'body', 'image']
+
+
+class DeleteReview(DeleteView):
+    """Delete the view that is shown"""
+    model = Post
+    template_name = 'delete-review.html'
+    fields = ['title', 'body', 'image']
+    success_url = reverse_lazy('home')
--- thereviewer/urls.py
+++ thereviewer/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('review/<int:pk>', views.DetailReview.as_view(), name='detail-review'),
     path('create-reviews/', views.CreateReview.as_view(), name='create-review'),
     path('update-reviews/<int:pk>', views.UpdateReview.as_view(), name='update-review'),
+    path('delete-reviews/<int:pk>', views.DeleteReview.as_view(), name='delete-review'),
 ]
