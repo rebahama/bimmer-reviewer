55444a10549cb3bc5a0d40ab93d7f286aa20a51a
rebahama <wiri@hotmail.se>
2022-07-06 02:25:12 +0000

    add script function for hideing and showing the created post elements

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+// shows and hide the elements
+let hide = document.querySelectorAll('.hide')
+let btn = document.getElementById('hide-btn');
+
+btn.addEventListener('click', function clickIt(){
+
+    for (let i = 0; i < hide.length; i++) {
+        if (hide[i].style.display==="none")
+        {
+            hide[i].style.display="block";
+        }
+        else{
+            hide[i].style.display="none"
+        }
+        
+        
+    }
+    
+})
