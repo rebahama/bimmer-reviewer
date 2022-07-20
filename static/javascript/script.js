e1a5ae92430e01b21ac24499b475ffa8f2b6c741
rebahama <wiri@hotmail.se>
2022-07-20 19:16:55 +0000

    add semicolon and fix w3 error on html files

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,63 +1,64 @@
 // Load this script after the html tag have been loaded
 window.onload = function(){
-// shows and hide the comments in the detail review page
-let btn = document.getElementById('hide-btn');
-if(btn){
-btn.addEventListener('click', clickIt)
-}
-function clickIt(){
-    let hide = document.querySelectorAll('.hide');
-    for (let i = 0; i < hide.length; i++) {
-        if (hide[i].style.display==="none")
-        {
-            hide[i].style.display="block";
-        }
-        else{
-            hide[i].style.display="none"
-        }   
+    // shows and hide the comments in the detail review page
+     'use strict';
+    let btn = document.getElementById('hide-btn');
+    if(btn){
+    btn.addEventListener('click', clickIt);
     }
-}
-// scroll back to top when clicked on this button
-let scrollBtn = document.getElementById('scroll-btn')
-if (scrollBtn){
-    scrollBtn.addEventListener('click', topClick)
-}
-
-function topClick(){
-    scrollBtn.style.backgroundColor="red"
-    document.body.scrollTop=0;
-    document.documentElement.scrollTop = 0;
-}
-
-let categoryBtn=document.getElementById('scroll-btn-category')
-if(categoryBtn){
-    categoryBtn.addEventListener('click', topClickCategory)
-}
-
-function topClickCategory(){
-    categoryBtn.style.backgroundColor="red"
-    document.body.scrollTop=0;
-    document.documentElement.scrollTop = 0;
-}
-
-//Change colour of cards when clicked
-let hideAll=document.getElementById('swipe-up-btn')
-let reveal=document.querySelectorAll('.swipe-up')
-if(reveal){
-    hideAll.addEventListener('click', scrollIt)
-}
-
-function scrollIt(){
-for (let i = 0; i < reveal.length; i++) {
-  if (reveal[i].style.display==="none")
-     reveal[i].style.display="block"
-  else
-  reveal[i].style.display="none"
-}
-}
-
-
-}
+    function clickIt(){
+        let hide = document.querySelectorAll('.hide');
+        for (let i = 0; i < hide.length; i++) {
+            if (hide[i].style.display==="none")
+            {
+                hide[i].style.display="block";
+            }
+            else{
+                hide[i].style.display="none";
+            }   
+        }
+    }
+    // scroll back to top when clicked on this button
+    let scrollBtn = document.getElementById('scroll-btn');
+    if (scrollBtn){
+        scrollBtn.addEventListener('click', topClick);
+    }
+    
+    function topClick(){
+        scrollBtn.style.backgroundColor="red";
+        document.body.scrollTop=0;
+        document.documentElement.scrollTop = 0;
+    }
+    
+    let categoryBtn=document.getElementById('scroll-btn-category');
+    if(categoryBtn){
+        categoryBtn.addEventListener('click', topClickCategory);
+    }
+    
+    function topClickCategory(){
+        categoryBtn.style.backgroundColor="red"
+        document.body.scrollTop=0;
+        document.documentElement.scrollTop = 0;
+    }
+    
+    //Change colour of cards when clicked
+    let hideAll=document.getElementById('swipe-up-btn');
+    let reveal=document.querySelectorAll('.swipe-up');
+    if(reveal){
+        hideAll.addEventListener('click', scrollIt);
+    }
+    
+    function scrollIt(){
+    for (let i = 0; i < reveal.length; i++) {
+      if (reveal[i].style.display==="none")
+         reveal[i].style.display="block";
+      else
+      reveal[i].style.display="none";
+    }
+    }
+    
+    
+    };
 
 
 
