370fd504c321709e97937150835f830f7c1d6c8a
rebahama <wiri@hotmail.se>
2022-07-21 22:58:39 +0000

    add variables for email send

--- thereviewer/settings.py
+++ thereviewer/settings.py
@@ -104,6 +104,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('SECRET_PASS')
 
 
 
